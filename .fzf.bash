7678d85aa8c6810e0842f345629a87dbd3d36e93
Evan Purcer <evanpurcer@gmail.com>
2018-12-06 22:32:41 +0000

    fzf hotkey from shell or vim

--- .fzf.bash
+++ .fzf.bash
@@ -15,3 +15,16 @@ source "$HOME/.fzf/shell/key-bindings.bash"
 # Make fzf respect .gitignore
 # ---------------------------
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+# Run fzf and open file in default editor on enter
+# create iterm2 hotkey mapped to [Send text: ":FZF \n"]
+# note: named ":FZF" so that same hotkey can activate fzf in vim
+:FZF() {
+  local out file key
+  IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
