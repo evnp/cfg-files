7e5eb8a6858c06af1864510aa9cc49e26b9ae915
Evan Purcer <evanpurcer@gmail.com>
2018-12-07 18:08:09 +0000

    fzf directory navigation

--- .fzf.bash
+++ .fzf.bash
@@ -12,19 +12,31 @@ fi
 # ------------
 source "$HOME/.fzf/shell/key-bindings.bash"
 
-# Make fzf respect .gitignore
+_fzf_find_files="rg --files"
+_fzf_find_directories="fd --type d --follow --exclude '.git' ."
+
+# fzf+ripgrep - fast & respects .gitignore
 # ---------------------------
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_DEFAULT_COMMAND=$_fzf_find_files
 
 # Run fzf and open file in default editor on enter
 # create iterm2 hotkey mapped to [Send text: ":FZF \n"]
 # note: named ":FZF" so that same hotkey can activate fzf in vim
 :FZF() {
-  local out file key
-  IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
-  key=$(head -1 <<< "$out")
+  local out file
+  IFS=$'\n' out=($(fzf --height 40% --query="$1" --exit-0))
   file=$(head -2 <<< "$out" | tail -1)
   if [ -n "$file" ]; then
-    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+    ${EDITOR:-vim} $file
+  fi
+}
+
+# Run fzf on directory list and open directory on enter
+fzfd() {
+  local out dir
+  IFS=$'\n' out=($(FZF_DEFAULT_COMMAND=$_fzf_find_directories fzf --height 40% --query="$1" --exit-0))
+  dir=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$dir" ]; then
+    cd $dir
   fi
 }
