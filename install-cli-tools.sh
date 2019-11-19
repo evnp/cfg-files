cf0259e90641ac5b49bc00bd8ea915fa973c392b
Evan Purcer <evan.purcer@mixpanel.com>
2019-11-19 01:33:35 +0000

    install cli tools script

---
+++ install-cli-tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ sudo apt-get update
+ sudo apt-get install ack-grep
+ sudo apt-get install fd-find
+ sudo apt-get install flake8
+ sudo apt-get install silversearcher-ag
+ sudo apt-get install tmux
+ sudo apt-get install vim
