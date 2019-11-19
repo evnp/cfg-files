7a29ec1e4fe7b45556aa98d13e3dbd55a8a5bc7e
Evan Purcer <evan.purcer@mixpanel.com>
2019-11-19 06:08:42 +0000

    more setup

--- install-cli-tools.sh
+++ install-cli-tools.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
- sudo apt-get update
- sudo apt-get install ack-grep
- sudo apt-get install fd-find
- sudo apt-get install flake8
- sudo apt-get install silversearcher-ag
- sudo apt-get install tmux
- sudo apt-get install vim
+sudo dpkg -i fd_7.4.0_amd64.deb
+
+sudo apt-get update
+sudo apt-get install ack-grep
+sudo apt-get install flake8
+sudo apt-get install silversearcher-ag
+sudo apt-get install tmux
+sudo apt-get install vim
